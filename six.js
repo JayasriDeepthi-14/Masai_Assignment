e33b57f88b6dabbf4228d5c571c998c85e2683c0
Sabbella Jayasri Deepthi <arunkumarreddymedapati@gmail.com>
2025-11-24 20:08:00 +0530

    Add files via upload

---
+++ six.js
@@ -0,0 +1,10 @@
+// Arrow Functions & Ternary Operator
+
+// a
+const isEven = n => n % 2 === 0;
+
+// b
+result = marks >= 35 ? "Pass" : "Fail";
+
+// c
+const greet = name => `Hello, ${name ? name : "Guest"}`;
