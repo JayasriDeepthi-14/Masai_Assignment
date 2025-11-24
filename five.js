07b2fedf7d411e158f4ae7b63a1cd768b3896fde
Sabbella Jayasri Deepthi <arunkumarreddymedapati@gmail.com>
2025-11-24 20:04:17 +0530

    Add files via upload

---
+++ five.js
@@ -0,0 +1,20 @@
+// Template Literals & Shorthands
+
+// a
+const username = "Sam";
+const course = "JavaScript";
+console.log(`Hello ${username}, welcome to the ${course}!`);
+
+// b
+const name = "Sam";
+const age = 21;
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};
+
+// c
+const getFullName = (first, last) => `${first} ${last}`;
